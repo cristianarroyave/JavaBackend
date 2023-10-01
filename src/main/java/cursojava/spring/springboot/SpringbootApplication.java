f43d19675d6b63657370b00961babd82db81b7e5
unknown <cristian.arroyave.clavijo@gmail.com>
2023-10-01 19:46:02 +0200

    Docker tomcat + postgresdb
    Añadir dockerfiles para imagenes de postgres + tomcat, modificar punto de entrada para poder arrancar en tomcat

--- src/main/java/cursojava/spring/springboot/SpringbootApplication.java
+++ src/main/java/cursojava/spring/springboot/SpringbootApplication.java
@@ -2,11 +2,12 @@ package cursojava.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
 
 	@Bean
 	public ModelMapper modelMapper(){
